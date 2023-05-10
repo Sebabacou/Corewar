4f6fa84399ee2596e8ef8da0ee4261cf118739ca
sebastien.bs <sebastien.bertilsouchet@protonmail.com>
2023-05-10 11:53:49 +0200

    fix(valgrind): leak_definity_lost

--- corewar/src/free/free_champ.c
+++ corewar/src/free/free_champ.c
@@ -10,8 +10,9 @@
 void free_champ(champion_t *champion, size_t nbr_champ)
 {
     if (champion != NULL) {
-        for (size_t i = 0; i != nbr_champ; i++) {
+        for (size_t i = 0; i != nbr_champ + 1; i++) {
             my_free((void *) champion[i].name);
+            my_free((void *) champion[i].process);
         }
         free(champion);
     }
