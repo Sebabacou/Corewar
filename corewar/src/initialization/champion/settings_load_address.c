289f9a2a4f0d774eb1e569bc5a3eb05e05f43852
3w3n <ewen.emeraud@epitech.eu>
2023-05-11 14:09:24 +0200

    fix(vm): begining of champ is fix

--- corewar/src/initialization/champion/settings_load_address.c
+++ corewar/src/initialization/champion/settings_load_address.c
@@ -10,13 +10,20 @@
 ssize_t manage_a(char **argv, size_t index, vm_t *vm)
 {
     ssize_t nbr = take_number(argv[index + 1]);
-    size_t pos_x = 0;
+    size_t x = 0;
+    UNUSED int check = 0;
+    int y = 0;
 
     if (nbr == -1)
         return -1;
-    for (pos_x = 0; nbr > MEM_Y; pos_x++)
-        nbr -= MEM_Y;
-    vm->champion[vm->champ_actu].process[0].pos_y = nbr;
-    vm->champion[vm->champ_actu].process[0].pos_x = pos_x;
+    for (int len = 0; len < nbr; len++) {
+        if (x == MEM_X) {
+            x = 0;
+            y++;
+        }
+        x++;
+    }
+    vm->champion[vm->champ_actu].process[0].pos_y = y - 1;
+    vm->champion[vm->champ_actu].process[0].pos_x = x - 1;
     return 1;
 }
