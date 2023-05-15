de1deab9becce22a8c5c5b68c6a250f821d8ef64
3w3n <ewen.emeraud@epitech.eu>
2023-05-15 11:50:38 +0200

    feat(vm): setup start of champ

--- corewar/src/initialization/init_startchamp.c
+++ corewar/src/initialization/init_startchamp.c
@@ -10,17 +10,23 @@
 void setup_start(vm_t *vm)
 {
     int dif = MEM_Y / vm->nbr_champ;
-    printf("dif = %d", dif);
+    int champ = 0;
+
+    for (int i = 0; i < MEM_Y; i += dif) {
+        vm->champion[champ].process[0]->pos_x = 0;
+        vm->champion[champ].process[0]->pos_y = i;
+        champ++;
+    }
 /*
     for (int i = 0; i != MEM_Y; i += dif) {
         vm->champion[champ].process[champ]->pos_x = 0;
         vm->champion[champ].process[champ]->pos_x = i;
     }*/
-    for (vm->champ_actu = 0; vm->champ_actu != vm->nbr_champ; vm->champ_actu++)
-        for (VM_CHAMP_ACTU.process_actu = 0; VM_CHAMP_ACTU.process_actu !=
-        VM_CHAMP_ACTU.nbr_of_process; VM_CHAMP_ACTU.process_actu ++) {
-            VM_PROCESS_ACTU->pos_x = 0;
-            VM_PROCESS_ACTU->pos_y = 0;
-        }
+    // for (vm->champ_actu = 0; vm->champ_actu != vm->nbr_champ; vm->champ_actu++)
+    //     for (VM_CHAMP_ACTU.process_actu = 0; VM_CHAMP_ACTU.process_actu !=
+    //     VM_CHAMP_ACTU.nbr_of_process; VM_CHAMP_ACTU.process_actu ++) {
+    //         VM_PROCESS_ACTU->pos_x = 0;
+    //         VM_PROCESS_ACTU->pos_y = 0;
+    //     }
     return;
 }
