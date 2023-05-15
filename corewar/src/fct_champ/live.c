485c2dd8db99581fd4e3448a8e06c3bde5f31b42
3w3n <ewen.emeraud@epitech.eu>
2023-05-15 20:27:25 +0200

    fix(vm): change live fct

--- corewar/src/fct_champ/live.c
+++ corewar/src/fct_champ/live.c
@@ -9,8 +9,8 @@
 
 int mooving_fct(champion_t *champion, int dist, int proc)
 {
-    int x = champion->process[proc].pos_x;
-    int y = champion->process[proc].pos_y;
+    int x = champion->process[proc]->pos_x;
+    int y = champion->process[proc]->pos_y;
 
     for (int len = 0; len < dist; len++) {
         if (x == MEM_X - 1) {
@@ -20,8 +20,8 @@ int mooving_fct(champion_t *champion, int dist, int proc)
         x++;
     }
 
-    champion->process[proc].pos_x = x;
-    champion->process[proc].pos_y = y;
+    champion->process[proc]->pos_x = x;
+    champion->process[proc]->pos_y = y;
     return 0;
 }
 
