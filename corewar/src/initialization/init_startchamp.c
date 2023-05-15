53ece5be31bc79204d99465b7bd43173d8f768af
3w3n <ewen.emeraud@epitech.eu>
2023-05-15 21:06:41 +0200

    feat(vm): init champ

--- corewar/src/initialization/init_startchamp.c
+++ corewar/src/initialization/init_startchamp.c
@@ -17,6 +17,12 @@ void setup_start(vm_t *vm)
         vm->champion[champ].process[0]->pos_y = i;
         champ++;
     }
+
+    int i = PROG_NAME_LENGTH  + COMMENT_LENGTH;
+    // for (; i != vm->champion[1].size_champion; i++)
+        // printf("%x|", vm->champion[1].buffer[i]);
+    printf("%ld\n", vm->champion[1].size_champion);
+    printf("%d \n", i);
 /*
     for (int i = 0; i != MEM_Y; i += dif) {
         vm->champion[champ].process[champ]->pos_x = 0;
