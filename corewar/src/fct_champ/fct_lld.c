dcd404c9ee639e4b94a3435734b069c992666d92
3w3n <ewen.emeraud@epitech.eu>
2023-05-22 16:33:16 +0200

    feat(vm) : add function lld

--- corewar/src/fct_champ/fct_lld.c
+++ corewar/src/fct_champ/fct_lld.c
@@ -9,6 +9,24 @@
 
 int fct_lld(vm_t *vm)
 {
+    int *tab;
+    size_t size_arg_one = get_arg_type(vm, 4);
+
+    if (size_arg_one == T_DIR) {
+        tab = new_pos(vm, 2);
+        size_t one = get_arg_value(vm, tab[0], tab[1], T_DIR);
+        tab = new_pos(vm, 4);
+        size_t id_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+        VM_PROCESS_ACTU->reg[id_reg] = one;
+    }
+
+    if (size_arg_one == T_IND) {
+        tab = new_pos(vm, 2);
+        size_t one = get_arg_value(vm, tab[0], tab[1], T_IND);
+        tab = new_pos(vm, 6);
+        size_t id_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+        VM_PROCESS_ACTU->reg[id_reg] = vm->buffer[VM_PROCESS_ACTU->pc + one];
+    }
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
