89a3232916ac50343f26c4ec5e151a4d9669c4a7
3w3n <ewen.emeraud@epitech.eu>
2023-05-22 16:32:26 +0200

    feat(vm): add function ld

--- corewar/src/fct_champ/fct_ld.c
+++ corewar/src/fct_champ/fct_ld.c
@@ -15,11 +15,24 @@
 
 int fct_ld(vm_t *vm)
 {
-//    size_t id_reg = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 2,
-//                                   VM_PROCESS_ACTU->pos_y, T_REG);
-//    size_t size_arg_one = get_arg_type(vm, 4);
-//    size_t one = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3,
-//                               VM_PROCESS_ACTU->pos_y, size_arg_one);
+    int *tab;
+    size_t size_arg_one = get_arg_type(vm, 4);
+
+    if (size_arg_one == T_DIR) {
+        tab = new_pos(vm, 2);
+        size_t one = get_arg_value(vm, tab[0], tab[1], T_DIR);
+        tab = new_pos(vm, 4);
+        size_t id_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+        VM_PROCESS_ACTU->reg[id_reg] = one;
+    }
+
+    if (size_arg_one == T_IND) {
+        tab = new_pos(vm, 2);
+        size_t one = get_arg_value(vm, tab[0], tab[1], T_IND);
+        tab = new_pos(vm, 6);
+        size_t id_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+        VM_PROCESS_ACTU->reg[id_reg] = vm->buffer[VM_PROCESS_ACTU->pc + one % IDX_MOD];
+    }
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
