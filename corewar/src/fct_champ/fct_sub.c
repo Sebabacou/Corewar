86d27db84a867b64d60d31c6b31f66d84a9c4c7c
3w3n <ewen.emeraud@epitech.eu>
2023-05-23 09:15:42 +0200

    feat(vm): add fct sub first version

--- corewar/src/fct_champ/fct_sub.c
+++ corewar/src/fct_champ/fct_sub.c
@@ -9,6 +9,16 @@
 
 int fct_sub(vm_t *vm)
 {
+    int *tab;
+    size_t size_arg_one = get_arg_type(vm, 4);
+
+    tab = new_pos(vm, 2);
+    size_t first_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+    tab = new_pos(vm, 3);
+    size_t second_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+    tab = new_pos(vm, 4);
+    size_t third_reg = get_arg_value(vm, tab[0], tab[1], T_REG);
+    VM_PROCESS_ACTU->reg[third_reg] = VM_PROCESS_ACTU->reg[first_reg] - VM_PROCESS_ACTU->reg[second_reg];
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
