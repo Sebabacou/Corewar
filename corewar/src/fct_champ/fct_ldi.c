89cfa769c4703948572ca147f311b1ed515b98b9
sebastien.bs <sebastien.bertilsouchet@protonmail.com>
2023-05-23 13:40:11 +0200

    fix(fct): ldi % IDX_MOD

--- corewar/src/fct_champ/fct_ldi.c
+++ corewar/src/fct_champ/fct_ldi.c
@@ -22,7 +22,7 @@ int fct_ldi(vm_t *vm)
 
     S += second_arg;
     VM_PROCESS_ACTU->reg[third_arg] = get_arg_value(vm, VM_PROCESS_ACTU->pc
-    + S % IDX_MOD, 0, REG_SIZE);
+    + (S % IDX_MOD), 0, REG_SIZE);
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
