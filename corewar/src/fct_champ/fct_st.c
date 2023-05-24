e89254ca008c6c8fbeeaee9795574b35cbf7fe2c
3w3n <ewen.emeraud@epitech.eu>
2023-05-24 11:06:48 +0200

    feat(vm) : add st and zjmp

--- corewar/src/fct_champ/fct_st.c
+++ corewar/src/fct_champ/fct_st.c
@@ -7,8 +7,33 @@
 
 #include "corewar.h"
 
+int do_st(vm_t *vm, size_t two, size_t id_reg)
+{
+    size_t x = 0;
+    size_t y = 0;
+
+    create_x_y_form_value((int *)&x, (int *)&y, VM_PROCESS_ACTU->pc + two % IDX_MOD);
+    printf("ST : value %d at pos %ld,%ld -> ", vm->buffer[y][x], y, x);
+    vm->buffer[y][x] = VM_PROCESS_ACTU->reg[id_reg];
+    printf("%d\n", vm->buffer[x][y]);
+    return 0;
+}
+
 int fct_st(vm_t *vm)
 {
+    size_t size_arg_one = get_arg_type(vm, 4);
+    size_t first_arg = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 2,
+                                     VM_PROCESS_ACTU->pos_y, size_arg_one);
+    size_t arg_size_two = get_arg_type(vm, 3);
+    size_t second_arg = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 2 + size_arg_one,
+                                      VM_PROCESS_ACTU->pos_y, arg_size_two);
+    
+    first_arg = VM_PROCESS_ACTU->reg[first_arg];
+
+    if (arg_size_two == T_REG)
+        VM_PROCESS_ACTU->reg[second_arg] = first_arg;
+    if (arg_size_two == T_IND)
+        do_st(vm, first_arg, second_arg);
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
--- corewar/src/fct_champ/fct_zjmp.c
+++ corewar/src/fct_champ/fct_zjmp.c
@@ -9,6 +9,12 @@
 
 int fct_zjmp(vm_t *vm)
 {
+    if (VM_PROCESS_ACTU->carry == 0)
+        return 0;
+    
+    size_t first_arg = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 2,
+                                     VM_PROCESS_ACTU->pos_y, IND_SIZE);
+    VM_PROCESS_ACTU->pc += first_arg % IDX_MOD;
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
