728d8d79f657de354df698e49329a06afbdec4bf
Marmotte <arthur.doriel@epitech.eu>
2023-05-25 00:15:11 +0200

    fix(fix if label = command): modif get_dif_label for the good size

--- asm/src/tools/get_dif_label.c
+++ asm/src/tools/get_dif_label.c
@@ -7,44 +7,56 @@
 
 #include "asm.h"
 
+char *clone_label(char *label)
+{
+    char *temp_label = my_strdup(label);
+    int i = my_strlen(label) - 1;
+
+    for (;i >= 1;i--) {
+        temp_label[i] = temp_label[i - 1];
+    }
+    temp_label[0] = ':';
+    return temp_label;
+}
+
 int check_label_before(t_command *node, char *buffer)
 {
     char *temp_command = NULL;
     int res = 0;
+    t_command *temp_node = node;
 
-    node = node->prev;
-    for (;node != NULL;node = node->prev) {
-        temp_command = my_clean_string(node->command,":",0);
+    temp_node = temp_node->prev;
+    for (;temp_node != NULL;temp_node = temp_node->prev) {
+        temp_command = clone_label(temp_node->command);
         if (temp_command == NULL)
             return 0;
         if (my_strcmp(temp_command, buffer) == 0) {
             free(temp_command);
             return res;
         }
-        res -= get_arg_size(node);
+        res -= get_arg_size(temp_node);
         free(temp_command);
     }
-    free(temp_command);
-    return 1;
+    return 0;
 }
 
 int check_label_after(t_command *node, char *buffer)
 {
     char *temp_command = NULL;
     int res = 0;
+    t_command *temp_node = node;
 
-    for (;node != NULL;node = node->next) {
-        temp_command = my_clean_string(node->command,":",0);
+    for (;temp_node != NULL;temp_node = temp_node->next) {
+        temp_command = clone_label(temp_node->command);
         if (temp_command == NULL)
             return 0;
         if (my_strcmp(temp_command, buffer) == 0) {
             free(temp_command);
             return res;
         }
-        res += get_arg_size(node);
+        res += get_arg_size(temp_node);
         free(temp_command);
     }
-    free(temp_command);
     return 0;
 }
 
@@ -53,10 +65,10 @@ int get_dif_label(t_command *node, char const *buffer)
     int res;
     char *temp_buffer = NULL;
 
-    temp_buffer = my_clean_string((char *)buffer, "%:", 0);
+    temp_buffer = my_clean_string((char *)buffer, "%", 0);
     if (temp_buffer == NULL)
         return 0;
-    if ((res = check_label_before(node, temp_buffer)) != 1) {
+    if ((res = check_label_before(node, temp_buffer)) != 0) {
         free(temp_buffer);
         return res;
     }
