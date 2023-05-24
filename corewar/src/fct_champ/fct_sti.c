6794e18ef2c3f2ac17e29a4fe6a07005ab6434ab
3w3n <ewen.emeraud@epitech.eu>
2023-05-24 11:56:01 +0200

    fix(vm): fix ccstyle and index bytes read

--- corewar/src/fct_champ/fct_sti.c
+++ corewar/src/fct_champ/fct_sti.c
@@ -17,23 +17,14 @@ int do_sti(vm_t *vm, size_t two, size_t three, size_t id_reg)
     printf("STI : value %d at pos %ld,%ld -> ", vm->buffer[y][x], y, x);
     vm->buffer[y][x] = VM_PROCESS_ACTU->reg[id_reg];
     printf("%d\n", vm->buffer[x][y]);
+    move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
 
-int fct_sti(vm_t *vm)
+size_t cond_sti(vm_t *vm, size_t size_arg_two, size_t two)
 {
-    size_t id_reg = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 2,
-                            VM_PROCESS_ACTU->pos_y, T_REG);
-    size_t size_arg_two = get_arg_type(vm, 2);
-    size_t two = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3,
-                            VM_PROCESS_ACTU->pos_y, size_arg_two);
-    size_t size_arg_three = get_arg_type(vm, 1);
-    size_t three = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3 + size_arg_two,
-                            VM_PROCESS_ACTU->pos_y, size_arg_three);
     size_t x = 0;
     size_t y = 0;
-//    printf("%ld | %ld\n", size_arg_two, size_arg_three);
-
     if (size_arg_two == T_REG)
         two = VM_PROCESS_ACTU->reg[two];
     if (size_arg_two == T_IND) {
@@ -41,9 +32,38 @@ int fct_sti(vm_t *vm)
         two % IDX_MOD);
         two = vm->buffer[y][x];
     }
+
+    return two;
+}
+
+size_t cond_sti_second(vm_t *vm, size_t size_arg_three, size_t two, size_t three)
+{
     if (size_arg_three == T_REG)
         two = VM_PROCESS_ACTU->reg[three];
+    return two;
+}
+
+int fct_sti(vm_t *vm)
+{
+    size_t two, three = 0;
+    size_t id_reg = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 2,
+                            VM_PROCESS_ACTU->pos_y, T_REG);
+    size_t size_arg_two = get_arg_type(vm, 3);
+    if (size_arg_two != T_REG)
+        two = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3,
+                            VM_PROCESS_ACTU->pos_y, IND_SIZE);
+    else
+        two = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3,
+                            VM_PROCESS_ACTU->pos_y, T_REG);
+    size_t size_arg_three = get_arg_type(vm, 2);
+    if (size_arg_three != T_REG)
+        three = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3 + size_arg_two,
+                            VM_PROCESS_ACTU->pos_y, IND_SIZE);
+    else
+        three = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 3 + size_arg_two,
+                            VM_PROCESS_ACTU->pos_y, T_REG);
+    two = cond_sti(vm, size_arg_two, two);
+    two = cond_sti_second(vm, size_arg_three, two, three);
     do_sti(vm, two, three, id_reg);
-    move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
