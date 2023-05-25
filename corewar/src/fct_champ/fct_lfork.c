5bf2c2eb295ae44ab7753aa8886643e344c8df43
sebastien.bs <sebastien.bertilsouchet@protonmail.com>
2023-05-25 22:24:57 +0200

    feat(fct): lfork

--- corewar/src/fct_champ/fct_lfork.c
+++ corewar/src/fct_champ/fct_lfork.c
@@ -9,6 +9,16 @@
 
 int fct_lfork(vm_t *vm)
 {
+    size_t parameter = get_arg_value(vm, VM_PROCESS_ACTU->pos_x + 1,
+                                     VM_PROCESS_ACTU->pos_y, DIR_SIZE);
+
+    VM_CHAMP_ACTU.process_actu++;
+    init_process(vm);
+    move_process(vm, VM_CHAMP_ACTU.process[VM_CHAMP_ACTU.process_actu -
+                                           1]->pc + parameter);
+    VM_PROCESS_ACTU->pc = VM_CHAMP_ACTU.process[VM_CHAMP_ACTU.process_actu -
+                                                1]->pc + parameter;
+    VM_CHAMP_ACTU.process_actu--;
     move_process(vm, VM_PROCESS_ACTU->pc);
     return 0;
 }
